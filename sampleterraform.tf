66779397053bc699e8caf2ebc0a31a9d73278faa
John Shelburne <shelburne@catfixtechnology.com>
2024-01-24 21:49:35 +0000

    terraform again

--- sampleterraform.tf
+++ sampleterraform.tf
@@ -1,7 +1,6 @@
-resource "google_storage_bucket" "frontend" {
+resource "google_storage_bucket" "frontend"
   name     = local.frontend_bucket_name
   location = var.region
   project  = var.project_id
   website {
-    main_page_suffix = "index.html"
-}
+    main_page_suffix = "index.html"}
