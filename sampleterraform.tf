78902e2d9e6bfcc1885fa2bee22cc4b70dfa151f
John Shelburne <shelburne@catfixtechnology.com>
2024-01-24 21:41:53 +0000

    trying to get terraform to work

---
+++ sampleterraform.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
